0371f5be2f6ef7414a113c8d1ba2c2758f308a03
정현동 <jeonghyeondong@MacBook-Pro.local>
2020-03-26 22:19:36 +0900

    todolist 테스트앱

--- webby/flutter_service_worker.js
+++ webby/flutter_service_worker.js
@@ -1,17 +1,22 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "/index.html": "4934c2de71727ef1c40d7f0b23371e13",
-"/main.dart.js": "9913de04e3f400e5799dd52b2178f994",
+  "/index.html": "9104ede5e9165be0aed3e14525cdeaef",
+"/main.dart.js": "0a6e396d6357fb3ae805e15ecd6ceab4",
 "/favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"/manifest.json": "73974f27b05f20b4cb13d13177ec9f50",
-"/assets/LICENSE": "964211db6a8b173b1744e68da77ce459",
-"/assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
-"/assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
+"/manifest.json": "8c189cd22f18289da34b642014992817",
+"/assets/LICENSE": "117087a03a7164a5668aeefde29060bd",
+"/assets/AssetManifest.json": "9e931c401a39e1e743c7ed0a7ef21671",
+"/assets/FontManifest.json": "3b425e2f698952f29bb78e3007676a64",
 "/assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
-"/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16"
+"/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
+"/assets/assets/background.png": "a5c631af551d21d3683a6a9e4c51cbf5",
+"/assets/assets/Nunito.ttf": "65bb0a158ee1967292ee4d11079d45ae",
+"/assets/assets/logo.png": "4231452532b8e057f50fb9457b8312b2",
+"/assets/assets/alucard.jpg": "13901053e0bb41ffd954addd234848f6",
+"/assets/assets/background-2.png": "490d2a692d5d9853aeca3ffe0ec76d4c"
 };
 
 self.addEventListener('activate', function (event) {
