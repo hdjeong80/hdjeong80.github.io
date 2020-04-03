3e09868797201b51f48ad01fbb124bcab088d778
hdjeong80 <hdjeong80@gmail.com>
2020-04-03 11:20:23 +0900

    v4

--- lunch_card/v4/flutter_service_worker.js
+++ lunch_card/v4/flutter_service_worker.js
@@ -5,13 +5,13 @@ const RESOURCES = {
 "/assets/assets/trophy.png": "a8b3db3405117366b30356aacf64490a",
 "/assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
 "/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"/assets/LICENSE": "6f4812ceaa5b653d2f4337883a823313",
+"/assets/LICENSE": "8ae29cc9e9fc480bd87df6b16c2cbb4c",
 "/assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
 "/favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "/index.html": "87f1b0a5351751c22e88ab6c58b0ae11",
-"/main.dart.js": "1189962cbb8dc0c857bae480c3ee044f",
+"/main.dart.js": "808c907985018d0a195df3c49e3ab3cc",
 "/manifest.json": "fdf29c499718f971e374b1965bc1550b"
 };
 
