e308ecdc56edfe25272b094fde74dac7a4999364
hdjeong80 <hdjeong80@gmail.com>
2020-10-30 11:22:14 +0900

    af

--- lunch_card/flutter_service_worker.js
+++ lunch_card/flutter_service_worker.js
@@ -14,9 +14,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "0923d299c931fe0631a98d292efe9706",
-"/": "0923d299c931fe0631a98d292efe9706",
-"main.dart.js": "5b82cb4a67fea7fcfdc6f06a57617b74",
+"index.html": "e91f995af6f15541de0f58e4e1f348ac",
+"/": "e91f995af6f15541de0f58e4e1f348ac",
+"main.dart.js": "20af368dd407e6eb88c48b0889d3ace0",
 "manifest.json": "fdf29c499718f971e374b1965bc1550b",
 "version.json": "698be64d7e08c15fb13defc0aa655ed6"
 };
